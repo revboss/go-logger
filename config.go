25e50b778455c08ee974ec4328e9ce2486c4c341
Colton Baker <github@netrefuge.net>
2016-06-01 09:55:02 -0400

    Initial commit

---
+++ config.go
@@ -0,0 +1,52 @@
+package logger
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Config struct {
+	Environment    string
+	Host           string
+	RollbarKey     string
+	PapertrailHost string
+	PapertrailPort int
+}
+
+func LoadConfig() (Config, error) {
+	c := Config{}
+
+	envs := map[string]interface{}{
+		"ENVIRONMENT":     &c.Environment,
+		"HOST":            &c.Host,
+		"ROLLBAR_KEY":     &c.RollbarKey,
+		"PAPERTRAIL_HOST": &c.PapertrailHost,
+		"PAPERTRAIL_PORT": &c.PapertrailPort,
+	}
+
+	var isProduction bool
+
+	for k, cv := range envs {
+		v := os.Getenv(k)
+		switch cv.(type) {
+		case *string:
+			if k == "ENVIRONMENT" && strings.ToLower(v) == "production" {
+				isProduction = true
+			}
+			if len(v) == 0 {
+				return c, fmt.Errorf("Missing required configuration value %s", k)
+			}
+			*cv.(*string) = v
+		case *int:
+			pv, _ := strconv.Atoi(v)
+			if pv == 0 && !(k == "LOG_PORT" && isProduction == false) {
+				return c, fmt.Errorf("Missing required configuration value %s", k)
+			}
+			*cv.(*int) = pv
+		}
+	}
+
+	return c, nil
+}
---
+++ logger.go
@@ -0,0 +1,38 @@
+package logger
+
+import (
+	"github.com/Sirupsen/logrus"
+	"github.com/heroku/rollrus"
+	"github.com/polds/logrus-papertrail-hook"
+	"github.com/stvp/roll"
+	"strings"
+)
+
+func New(app string) *logrus.Logger {
+	log := logrus.New()
+
+	conf, e := LoadConfig()
+	if e != nil {
+		log.Fatal(e)
+	}
+
+	le := strings.ToLower(conf.Environment)
+	if le == "production" || le == "staging" {
+		log.Hooks.Add(&rollrus.Hook{
+			Client: roll.New(conf.RollbarKey, conf.Environment),
+		})
+
+		phook, err := logrus_papertrail.NewPapertrailHook(&logrus_papertrail.Hook{
+			Host:     conf.PapertrailHost,
+			Port:     conf.PapertrailPort,
+			Hostname: conf.Host,
+			Appname:  app,
+		})
+
+		if err == nil {
+			log.Hooks.Add(phook)
+		}
+	}
+
+	return log
+}
