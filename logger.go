0eb1a9f124fd82dfe21f3c2b7ab660dabbeb948f
Colton Baker <github@netrefuge.net>
2016-07-20 18:43:08 -0400

    Use revboss/go-config for configuration

--- logger.go
+++ logger.go
@@ -5,20 +5,59 @@ import (
 	"github.com/Sirupsen/logrus"
 	"github.com/heroku/rollrus"
 	"github.com/polds/logrus-papertrail-hook"
+	"github.com/revboss/go-config"
 	"github.com/stvp/roll"
 	"strings"
 )
 
+type Config struct {
+	Environment    string
+	Host           string
+	RollbarKey     string
+	PapertrailHost string
+	PapertrailPort int
+}
+
 func New(app string) *logrus.Logger {
 	log := logrus.New()
 
-	conf, e := LoadConfig()
+	conf := &Config{}
+	e := config.LoadConfig(config.Config{
+		Env: map[string]config.Value{
+			"ENVIRONMENT": {
+				Default: "development",
+				Value:   &conf.Environment,
+			},
+		},
+	})
 	if e != nil {
 		log.Fatal(e)
 	}
 
 	le := strings.ToLower(conf.Environment)
 	if le == "production" || le == "staging" {
+		e := config.LoadConfig(config.Config{
+			Env: map[string]config.Value{
+				"HOST": {
+					Value: &conf.Host,
+				},
+				"ROLLBAR_KEY": {
+					Value: &conf.RollbarKey,
+				},
+				"PAPERTRAIL_HOST": {
+					Default: "logs2.papertrailapp.com",
+					Value:   &conf.PapertrailHost,
+				},
+				"PAPERTRAIL_PORT": {
+					Default: "33263",
+					Value:   &conf.PapertrailPort,
+				},
+			},
+		})
+		if e != nil {
+			log.Fatal(e)
+		}
+
 		log.Hooks.Add(&rollrus.Hook{
 			Client: roll.New(conf.RollbarKey, conf.Environment),
 		})
